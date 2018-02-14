3e40ff37a895b1c662e6320d47812a5a28e572c2
BonnW <pwonghansa@gmail.com>
2018-02-14 15:24:43 -0600

    some notes

---
+++ Theory of Computing and State Machines/AlgorithmsAndBigO.js
@@ -0,0 +1,15 @@
+/**    Big 'O Notation   **/
+// some notes in notebook
+
+// You can think of an Algorithm as something that gives you 
+// a recipe step by step to try to solve some sort of problem
+// I.E. take some input and gives some output
+
+
+/**
+ *  Big O' is a Measure of runtime Efficiency 
+ *  Asymptotic = 
+ * 
+ * 
+ * 
+**/
---
+++ Theory of Computing and State Machines/Feb14.js
@@ -0,0 +1,24 @@
+/** Stuff about Lambda Calculus */
+
+/** 
+    Lambda Calculus created by: Alonzo Church
+    A Mathmatical model for computation that involves a lot of 
+    functions and recursion
+
+    * Turing's invention of the Turing Machine and Alonzo Church's invention
+    * of the Lambda Calculus happened at a very similar time
+    * --> Turing then added an addendum to his paper stating that these two 
+    * --> methods of computation are equivilent. 
+
+*/
+
+/**  Boolean Algebra  **/
+
+/** 
+ *  A v B (A or B)
+ *  A ^ B (A and B)
+ *  ¬A (not A)
+ *  A => B (A implies B)
+ *  A <=> B (A if and only if B)
+ * 
+*/
