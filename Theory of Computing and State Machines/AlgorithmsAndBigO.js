6b3a9ceea178fa639bad1d64268d2efd414ca7d6
Bonn Wonghansa <pwonghansa@gmail.com>
2018-02-14 21:35:59 -0600

    another commit

--- Theory of Computing and State Machines/AlgorithmsAndBigO.js
+++ Theory of Computing and State Machines/AlgorithmsAndBigO.js
@@ -8,8 +8,17 @@
 
 /**
  *  Big O' is a Measure of runtime Efficiency 
- *  Asymptotic = 
- * 
- * 
+ *  Asymptotic = Assuming You keep throwing data at it, what happens
+ *  Don't care about constants 
+ *  Care about the things that you run that grow as you throw more 
+ *  input at your program. 
  * 
 **/
+
+/**  O(1)  **/
+// Uses a Constant
+// Constants are free
+
+/**   O(log n)   **/
+//
+
